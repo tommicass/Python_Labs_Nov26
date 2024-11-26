67cb8044b29db04dbf1d88d0751e0c1edb67608b
Donald Cameron <donald.cameron@ntlworld.com>
2024-11-26 17:25:07 +0000

    Added lotto demo

---
+++ Trainer Demos/demo_lotto.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+# Author: DCameron
+# Version: 1.0
+# Description: This script will generate 6 unique random lottery
+# numbers
+import random
+
+lotto = []
+
+while len(lotto) < 6:
+    num = random.randint(1, 50)
+    if num not in lotto:
+        lotto.append(num)
+    else: 
+        print("Duplicate number =", num)
+
+
+print("Lottery numbers =", lotto)
