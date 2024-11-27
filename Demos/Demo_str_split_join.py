8d38a93fdccaa2c9a753c33abe63e55fd321d141
Tom Cassidy <tommicass@hotmail.co.uk>
2024-11-27 13:48:15 +0000

    Initial string added

--- Demos/Demo_str_split_join.py
+++ Demos/Demo_str_split_join.py
@@ -1,12 +1,15 @@
 #! user/bin/env Python3
 # Author: TCassidy
 # Version: 1.0
-# Description: This script will demo
+# Description: This script will demo how to split and rejoin a string
 """
 Docstring
 """
 import sys
 
-# new code here
+# Sample line from etc/passwd on Linux for the root user account
+line = r"root:x:0:0:The Super User:/root:/bin/ksh"
+
+
 
 sys.exit(0)
