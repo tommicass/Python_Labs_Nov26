2c890485752e8f2e30a7ad85c98941b484c3ed50
Tom Cassidy <tommicass@hotmail.co.uk>
2024-11-27 10:50:54 +0000

    Added script for pin machine

---
+++ Demos/Demo_Pin.py
@@ -0,0 +1,23 @@
+#! usr/bin/env python3
+#Author: T. Cassidy
+#Version: 1.0
+#Description: This script will simulate a high street bank pin machine
+
+master_pin = "0123"
+pin = None
+attempts = 0
+
+while pin != master_pin and attempts < 3:
+    pin = input("Enter Pin: ")
+    if pin == master_pin:
+        print("Valid PIN")
+        break # break prevents else statement from executing - drops out of while loop
+    else:
+        print("Invalid PIN")
+        attempts += 1
+else: 
+    #Executes only once when while condition becomes false
+    print("Too many attempts")
+    print("Card retained")
+
+print("Done")
