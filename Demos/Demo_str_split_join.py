470bf2edf498840ec9124038c4d32f73ede665d4
Tom Cassidy <tommicass@hotmail.co.uk>
2024-11-27 13:52:48 +0000

    Finished demo

--- Demos/Demo_str_split_join.py
+++ Demos/Demo_str_split_join.py
@@ -10,6 +10,19 @@ import sys
 # Sample line from etc/passwd on Linux for the root user account
 line = r"root:x:0:0:The Super User:/root:/bin/ksh"
 
+fields = line.split(":") # Returns a list - list can be editted
 
+print(fields)
+
+# Change fields 4 + 6
+fields[4] = "The Administrator"
+fields[6] = "bin/bash"
+
+print(fields)
+
+# Join list back into a string but using : as delimiter
+line = ":".join(fields)
+
+print(line)
 
 sys.exit(0)
