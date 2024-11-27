13c319478f0b95061868ea9dd2f98f81bade53a6
Tom Cassidy <tommicass@hotmail.co.uk>
2024-11-27 15:57:20 +0000

    Create dictionary demo

---
+++ Demos/Demo_dictionary_movies.py
@@ -0,0 +1,12 @@
+#! user/bin/env Python3
+# Author: TCassidy
+# Version: 1.0
+# Description: This script will demo how to create, grow, shrink and access keys and values in a dictionary with unique keys
+"""
+Docstring
+"""
+import sys
+
+
+
+sys.exit(0)
