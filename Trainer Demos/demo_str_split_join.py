f2578c439695eda29c6ebc6526796e52a39efe84
Donald Cameron <donald.cameron@ntlworld.com>
2024-11-27 13:53:23 +0000

    Added str split and join demo

--- Trainer Demos/demo_str_split_join.py
+++ Trainer Demos/demo_str_split_join.py
@@ -1,13 +1,22 @@
 #! /usr/bin/env python3
 # Author: DCameron
 # Version: 1.0
-# Description: This script will demo
+# Description: This script will demo HOWWTO split and rejoin a string.
 """
   Docstring
 """
 import sys
-# New code HERE
 
+# Sample line from /etc/passwd on Linux for the root user account
+line = r"root:x:0:0:The Super User:/root:/bin/ksh"
+
+# ..and I want to modify parts of the str! IMMUTABLE!
+fields = line.split(":") # Returns a LIST which is MUTABLE!
+fields[4] = "The Administrator"
+fields[6] = "/bin/bash"
+
+line = ":".join(fields)  # Return a new STR
+print(line)
 
 
 
