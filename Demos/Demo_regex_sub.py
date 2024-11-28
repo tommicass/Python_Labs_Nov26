6b7f1453628c72e0958deace9b2106671211688d
Tom Cassidy <tommicass@hotmail.co.uk>
2024-11-28 10:39:17 +0000

    Added regex sub demo

---
+++ Demos/Demo_regex_sub.py
@@ -0,0 +1,12 @@
+#! user/bin/env Python3
+# Author: TCassidy
+# Version: 1.0
+# Description: This script will demo how to match and substitute regex patterns using the re.sub() and re.subn() functions
+"""
+Docstring
+"""
+import sys
+
+# new code here
+
+sys.exit(0)
