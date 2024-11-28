975fbd589f84f6e11473711dfae65e76c059219d
Tom Cassidy <tommicass@hotmail.co.uk>
2024-11-28 08:57:07 +0000

    Created regex demo

---
+++ Demos/Demo_Regex.py
@@ -0,0 +1,9 @@
+#! user/bin/env Python3
+# Author: TCassidy
+# Version: 1.0
+# Description: This script will demo how to match text data using string testing and regular expressions (regex) and the re module
+"""
+Docstring
+"""
+import re
+
