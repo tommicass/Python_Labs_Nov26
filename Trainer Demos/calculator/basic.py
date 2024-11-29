167e0a55eb9403e0038278845b353fb0c8e829ab
Donald Cameron <donald.cameron@ntlworld.com>
2024-11-29 16:29:43 +0000

    Added DocString Tests to basic.py

--- Trainer Demos/calculator/basic.py
+++ Trainer Demos/calculator/basic.py
@@ -7,7 +7,12 @@
 """
 
 def add(*args):
-    """ Return SUM of all arguments """
+    """ Return SUM of all arguments 
+    >>> add(4, 3)
+    7
+    >>> add(4, 3, 2)
+    9
+    """
     total = 0
     for num in args:
         total += num
@@ -18,13 +23,24 @@ def sub(x, z):
     return x-z
 
 def mul(*args):
-    """ Return PRODUCT of all the arguments """
+    """ Return PRODUCT of all the arguments 
+    >>> mul(4, 3)
+    12
+    >>> mul(4, 3, 2)
+    24
+    """
     total = 1
     for num in args:
         total *= num
     return total
 
 def div(x, z):
-    """ Return quotient of x divided by z to 3 decimal places """
+    """ Return quotient of x divided by z to 3 decimal places 
+    >>> div(4, 3)
+    1.333
+    """
     return round(x/z, 3)
 
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
