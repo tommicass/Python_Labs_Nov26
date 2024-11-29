86a5e4e55358517e23472f945790ceacfc631487
Tom Cassidy <tommicass@hotmail.co.uk>
2024-11-29 16:29:43 +0000

    Updated basic calculator with doctest example

--- Demos/Calculator/Basic.py
+++ Demos/Calculator/Basic.py
@@ -8,7 +8,12 @@ Basic calculator functions including add, sub, mul and div functions
 import sys
 
 def add(*args):
-    """ Return SUM of all arguments """
+    """ Return SUM of all arguments 
+    >>> add(4, 3)
+    7
+    >>> add(4, 3, 2)
+    9
+    """
     total = 0
     for num in args:
         total += num
@@ -19,16 +24,26 @@ def sub(x, z):
     return x - z
 
 def mul(*args):
-    """ Return product of all the arguments """
+    """ Return product of all the arguments 
+    >>> mul(4, 3)
+    12
+    >>> mul(4, 3, 2)
+    24
+    """
     total = 1
     for num in args:
         total *= num
     return total
 
 def div(x, z):
-    """ Return divisor of x divided by z to 3 decimal places """
+    """ Return divisor of x divided by z to 3 decimal places 
+    >>> div(4, 3)
+    1.333
+    """
     return round(x/z, 3)
 
 if __name__ == "__main__":
     # Include standalone code to run if running as a program (not a module)
-    sys.exit(0)
+    import doctest
+    doctest.testmod()
+    #sys.exit(0)
