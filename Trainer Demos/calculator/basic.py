ffaeb4d6b509451c96245514e0b2f1d183fa8392
Donald Cameron <donald.cameron@ntlworld.com>
2024-11-29 11:43:06 +0000

    Added Basic module to calculator package

---
+++ Trainer Demos/calculator/basic.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python3
+# Author: DCameron
+# Version: 1.0
+# Description: This module defines a collection of calculator functions
+"""
+  Basic Calcualtor functions including add, sub, mul and div functions.
+"""
+import sys
+
+def add(*args):
+    """ Return SUM of all arguments """
+    total = 0
+    for num in args:
+        total += num
+    return total
+
+def sub(x, z):
+    """ Retuen result of x subtract z """
+    return x-z
+
+def mul(*args):
+    """ Return PRODUCT of all the arguments """
+    total = 1
+    for num in args:
+        total *= num
+    return total
+
+def div(x, z):
+    """ Return quotient of x divided by z to 3 decimal places """
+    return round(x/z, 3)
+
+
+sys.exit(0)
