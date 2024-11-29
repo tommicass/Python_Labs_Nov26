1a935f635346351f61fc5139de7cefd18c8c9306
Tom Cassidy <tommicass@hotmail.co.uk>
2024-11-29 11:47:59 +0000

    Added advanced module

---
+++ Demos/Calculator/Adv.py
@@ -0,0 +1,22 @@
+#! user/bin/env Python3
+# Author: TCassidy
+# Version: 1.0
+# Description: This module defines a set of advanced calculator functions
+"""
+Advanced functions for calculator app including modulus, power & sqrt
+"""
+import sys
+
+def mod(x, z):
+    """ Return remained for x modulus z """
+    return x % z
+
+def power(x, z):
+    """ Return power of x to z as a float """
+    return float(x**z)
+
+def sqrt(x):
+    """ Return the square root of x as a float to 2 decimal places"""
+    return round(x**0.5, 2)
+
+sys.exit(0)
